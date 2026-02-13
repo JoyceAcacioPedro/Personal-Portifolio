3f3318a015c26b996e81a68781931a5fd516fee4
Joyce Acácio Pedro <168125632+JoyceAcacioPedro@users.noreply.github.com>
2026-02-13 16:23:24 +0200

    Adding privacy.html page

--- portifolio/src/App.jsx
+++ portifolio/src/App.jsx
@@ -12,6 +12,7 @@ import Projects from './pages/Projects';
 import Contact from './pages/Contact'; 
 import Blog from './pages/Blog'; 
 import Resume from './pages/Resume'; 
+import Privacy from "./Privacy";
 
 function App() {
 
@@ -28,6 +29,8 @@ function App() {
           <Route path="/contact" element={<Contact />} /> 
           <Route path="/blog" element={<Blog />} /> 
           <Route path="/resume" element={<Resume />} /> 
+          <Route path="/privacy" element={<Privacy />} />
+
         </Routes>
       </main>
       
